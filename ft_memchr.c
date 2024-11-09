48a9ed9cbdcce53df5fdd1ebb22c0bccc26d35f3
irolaizo <irolaizo@student.42urduliz.com>
2024-11-09 18:04:24 +0100

    My final code minitalk ⚡️ :art:

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
